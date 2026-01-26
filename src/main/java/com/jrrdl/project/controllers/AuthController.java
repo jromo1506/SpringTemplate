769d38459f7b411546b98f845d29f06bf40c2b8e
Jorge <jromo1506mx@gmail.com>
2026-01-26 16:46:11 -0600

    Solucion filtro y chain

--- src/main/java/com/jrrdl/project/controllers/AuthController.java
+++ src/main/java/com/jrrdl/project/controllers/AuthController.java
@@ -3,17 +3,28 @@ package com.jrrdl.project.controllers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import com.jrrdl.project.dtos.auth.AuthResponse;
 import com.jrrdl.project.dtos.auth.LoginRequest;
+import com.jrrdl.project.dtos.user.UserRequest;
+import com.jrrdl.project.dtos.user.UserResponse;
 import com.jrrdl.project.services.AuthService;
+import com.jrrdl.project.services.UserService;
 
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
     
     private final AuthService authService;
+    private final UserService userService;
 
-    public AuthController(AuthService authService) {
+    public AuthController(AuthService authService,UserService userService) {
         this.authService = authService;
+        this.userService = userService;
     }
 
     @PostMapping("/login")
@@ -21,4 +32,11 @@ public class AuthController {
             @Valid @RequestBody LoginRequest request) {
         return ResponseEntity.ok(authService.login(request));
     }
+
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> register(@RequestBody UserRequest newUser){
+        return ResponseEntity.ok(userService.create(newUser));
+    }   
+    
 }
